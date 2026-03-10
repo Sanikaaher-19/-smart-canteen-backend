9fcdf0554a62d078d2ac631e86c9682041ce86e1
unknown <sanikaaher21@gmail.com>
2026-03-10 21:02:15 +0530

    updated coors

--- src/main/java/canteen_bite/example/Canteen_bite/config/WebMvcConfig.java
+++ src/main/java/canteen_bite/example/Canteen_bite/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package canteen_bite.example.Canteen_bite.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Value("frontend.url")
+    private String frontendurl;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow CORS on all endpoints
-                .allowedOrigins("http://localhost:3000") // React app origin
+                .allowedOrigins(frontendurl) // React app origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow these HTTP methods
                 .allowedHeaders("*") // Allow all headers (including Authorization)
                 .allowCredentials(true); // Allow cookies/authorization headers
